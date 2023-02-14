1a2e4a136f8954a74cb197fed61c612b4c89e45d
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-02-14 15:36:40 +0100

    Basic tekst formattering og API

--- client/src/App.js
+++ client/src/App.js
@@ -13,16 +13,48 @@ function App() {
     )
   }, []);
 
+  let title = (
+    <h1>{backendData.book}</h1>
+  )
+
+  let lines = [];
+
+  let verses = backendData.verses;
+  let titles = backendData.titles;
+
+  if (verses !== undefined) {
+    verses.map((verse, i) => {
+      let title = titles.find(({verse}) => 
+      {
+        return verse === "" + (i+1);
+      })
+      if (title !== undefined) {
+        lines.push({type: "subtitle", text: title.text});
+      }
+      lines.push({type: "verse", text: ("v" + (i+1) + " " + verse)}); 
+    });
+  }
+
+  let text = (
+    (lines === 'undefined') ? (
+      <p>Loading...</p>
+    ) : (
+      lines.map((line, i) => {
+          switch(line.type) {
+            case "subtitle":
+              return (<h2>{line.text}</h2>);
+            case "verse":
+              return (<span>{line.text}</span>);
+          }
+        }
+      )
+    )
+  )
+
   return (
     <div>
-      <h1>Brugere</h1>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        backendData.users.map((user, i) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
+      {title}
+      {text}
     </div>
   )
 }
