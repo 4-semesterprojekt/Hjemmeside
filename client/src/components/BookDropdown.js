16ff8e6ab6c321136d0d6afe3e774f188a2e1dfb
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-03-06 10:03:20 +0100

    Fiksede bug med pile

--- client/src/components/BookDropdown.js
+++ client/src/components/BookDropdown.js
@@ -43,14 +43,24 @@ function BookDropdown(props) {
         setCurrentChapter(chapter);
     }
 
+    function addChapter() {
+        if (currentChapter < currentBook.chapters)
+            onChapterChange(parseInt(currentChapter) + 1);
+    }
+
+    function subtractChapter() {
+        if (currentChapter > 1)
+            onChapterChange(parseInt(currentChapter) - 1);
+    }
+
     return (
         // Sticky top: https://stackoverflow.com/questions/28340054/bootstrap-keep-div-fixed-after-scrolling-to-it
         <div className='sticky-top'>
             <div className="container px-0 mx-0 mb-2">
                 <div className='arrow-container'>
                     <div className='d-flex justify-content-between'>
-                        <a className='arrow arrow-left ignore' href='#/' onClick={() => { onChapterChange(currentChapter > 1 ? (currentChapter - 1) : 1) }}>←</a>
-                        <a className='arrow arrow-right ignore' href='#/' onClick={() => { onChapterChange(currentChapter < currentBook.chapters ? (currentChapter + 1) : currentBook.chapters) }}>→</a>
+                        <a className='arrow arrow-left ignore' href='#/' onClick={subtractChapter}>←</a>
+                        <a className='arrow arrow-right ignore' href='#/' onClick={addChapter}>→</a>
                     </div>
                 </div>
                 <div className="row">
@@ -74,51 +84,5 @@ function BookDropdown(props) {
         </div>
     )
 }
-
-/*class BookDropdown extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            books: props.books
-        }
-
-        this.booksChanged = this.booksChanged.bind(this);
-        this.chapterChanged = this.chapterChanged.bind(this);
-    }
-
-    booksChanged(e) {
-        this.props.bookChanged(e.target.value);
-    }
-
-    chapterChanged(e) {
-        this.props.chapterChanged(e.target.value);
-    }
-
-    render() {
-        return (<div className="container px-0 mb-4 mx-0">
-            <div className="row">
-                <div className='col-8'>
-                    <select className="form-select form-select-lg" onChange={this.booksChanged} name="books" id="books">
-                        {
-                            (this.state.books !== undefined) ? (this.state.books.map((book, i) => 
-                                <option key={i} value={book.abbreviation}>{book.name}</option>
-                            )) : ""
-                        }
-                    </select>
-                </div>
-                <div className='col-4'>
-                    <select className="form-select form-select-lg" onChange={this.chapterChanged} name="chapter" id="chapter">
-                        {
-                            (this.state.books !== undefined) ? (this.state.books.map((book, i) => 
-                                <option key={i} value={book.abbreviation}>{book.name}</option>
-                            )) : ""
-                        }
-                    </select>
-                </div>
-            </div>
-        </div>);
-    }
-}*/
 
 export default BookDropdown;
--- client/src/components/Verse.js
+++ client/src/components/Verse.js
@@ -19,9 +19,9 @@ function Verse(props) {
                 {
                     const tooltipID = props.verseNumber + "f" + props.footnotes[j].designation;
                     const footnote = (
-                        <span>
+                        <span key={i}>
                             <Tooltip id={tooltipID}/>
-                            <sup key={i} className='ignore'>
+                            <sup className='ignore'>
                                 [<a href="#/" data-tooltip-id={tooltipID} data-tooltip-content={props.footnotes[j].text}>{props.footnotes[j].designation}</a>]
                             </sup>
                         </span>
